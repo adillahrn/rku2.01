04f0a8d9aeddf988bfc218baab990e90b999c96c
adillahrn <adillahridwan24@gmail.com>
2026-05-30 20:46:10 +0700

    initial commit

---
+++ main_menu.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_continue_pressed() -> void:
+	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_how_to_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://htp.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
